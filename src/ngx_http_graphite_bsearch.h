0dee78a4ca11031fde2c15a52dffd8ec9900b54e
Alexander Drozdov <al.drozdov@gmail.com>
2020-07-03 18:06:44 +0300

    add a binary search algorithm implementation
    Introduce ngx_graphite_bsearch() function which provides a binary search
    algorithm implementation.

---
+++ src/ngx_http_graphite_bsearch.h
@@ -0,0 +1,30 @@
+#ifndef _NGX_HTTP_GRAPHITE_BSEARCH_H_INCLUDED_
+#define _NGX_HTTP_GRAPHITE_BSEARCH_H_INCLUDED_
+
+#include <stddef.h>
+
+static inline void *
+ngx_graphite_bsearch(
+    const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *), int *found) {
+
+    size_t l = 0, r = nmemb;
+
+    while (l < r) {
+        size_t m = l + (r - l) / 2;
+
+        int c = compar(key, (char*)base + m * size);
+        if (c < 0)
+            r = m;
+        else if (c > 0)
+            l = m + 1;
+        else {
+            *found = 1;
+            return (void*)((char*)base + m * size);
+        }
+    }
+
+    *found = 0;
+    return (void*)((char*)base + l * size);
+}
+
+#endif
